60fb971a4302aa54225d84ff0e10bc15a67a2f94
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-11 16:57:22 +0500

    first commit

---
+++ js/hide lists.js
@@ -0,0 +1,19 @@
+// !==================== Function hide list BUTTON ====================
+function hide(elementId) {
+            let list = document.getElementById(elementId);
+            if (list.style.display === "none") {
+                list.style.display = "block";
+            } else {
+                list.style.display = "none";
+            }
+        }
+
+// !==================== KATALOG PAGE ====================
+function hide_katalog(elementId) {
+    let list0 = document.getElementById(elementId);
+    if (list0.style.display === "none") {
+        list0.style.display = "block";
+    } else {
+        list0.style.display = "none";
+    }
+}
